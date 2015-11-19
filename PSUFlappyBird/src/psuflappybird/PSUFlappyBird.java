5a68e3816fbd3c742fd7f9dee4426deab2fa8444
lzm5268 <lzm5268@206-L47.up.ist.local>
2015-11-18 19:25:55 -0500

    logan 725

--- PSUFlappyBird/src/psuflappybird/PSUFlappyBird.java
+++ PSUFlappyBird/src/psuflappybird/PSUFlappyBird.java
@@ -26,42 +26,8 @@ public class PSUFlappyBird {
     
     public static void main(String[] args) {
 
-        //Home g = new Home(); 
-        //g.setVisible(true);
         
         MainFrame mainFrame = new MainFrame();
-        
-       // PipeFrame thisPipeFrame = new PipeFrame();
-
-        //PipeFrame thisPipeFrame = new PipeFrame();
-        //BirdFrame birdF = new BirdFrame();
-
-        //h.setVisible(true);
-       
-        //BirdFrame birdF = new BirdFrame();
-
-
-        
-        BirdFrame birdF = new BirdFrame();
-
-     
-
-        
-        //thisPipe.draw(g);
-        // TODO code application logic here
+   
     }
 }
-
-       // PipeFrame thisPipeFrame = new PipeFrame();
-        
-        
-        //thisPipe.draw(g);
-        // TODO code application logic here
-
-
-    
-    
-    
-    
-
-
